d4bf0733fee4ab4d85f5dea79e7a65e4ed188595
NatashaBazunova <natashaiosifova@mail.ru>
2022-03-08 21:07:54 +0500

    passedTest commit

--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -17,4 +17,26 @@ class StatisticServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void findMax2() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMax3() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {22, 45, 89, 1, 33, 41};
+        long expected = 89;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
